15823bc03fe15495d197781629a0bde40815570d
lblissett <luther.blissett@posteo.net>
2016-11-27 22:08:07 +0100

    - minor changes

--- src/data/allInOne_Script.R
+++ src/data/allInOne_Script.R
@@ -18,21 +18,21 @@ g20 + theme() + theme(panel.grid.major = element_line(color = "black",linetype =
 ggsave("../best-20-stable.jpeg")
 
 
-g50 <- ggplot(data=mydata50, aes(x=mydata50$Zyklus, color=Kodierung)) + geom_line(aes(y=mydata50$bester.Fitnesswert.Real, col="Reele Werte")) + geom_line(aes(y=mydata50$Binaer1P, col="Binär1P"))+ geom_line(aes(y=mydata50$Binaer2P, col = "Binär2P")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
+g50 <- ggplot(data=mydata50, aes(x=mydata50$Zyklus, color=Kodierung)) + geom_line(aes(y=mydata50$bester.Fitnesswert.Real, col="Reelle Werte")) + geom_line(aes(y=mydata50$Binaer1P, col="Binär1P"))+ geom_line(aes(y=mydata50$Binaer2P, col = "Binär2P")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
 g50 + theme() + theme(panel.grid.major = element_line(color = "black",
 linetype = "dotted"), legend.position=c(.80, .90)) + labs(x = "Zyklus",y = "Fitnesswert", title = "Mittlerer Fitnesswertverlauf mit 50 Genen (stabile Population)")
 
-g5worst <- ggplot(data=mydata5, aes(x=mydata5$Zyklus, color=Kodierung))  + geom_line(aes(y=mydata5$worstBinaer1P, col="worstBinaer1P"))+ geom_line(aes(y=mydata5$worstBinaer2P, col="worstBinaer2P")) + geom_line(aes(y=mydata5$worstReal, col="worstReel")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
+g5worst <- ggplot(data=mydata5, aes(x=mydata5$Zyklus, color=Kodierung))  + geom_line(aes(y=mydata5$worstBinaer1P, col="worstBinaer1P"))+ geom_line(aes(y=mydata5$worstBinaer2P, col="worstBinaer2P")) + geom_line(aes(y=mydata5$worstReal, col="worstReal")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
 g5worst + theme() + theme(panel.grid.major = element_line(color = "black",
 linetype = "dotted"), legend.position = c(0.80,0.90)) + labs(x = "Zyklus",y =
  "Fitnesswert", title = "Mittlerer Fitnesswertverlauf mit 5 Genen (stabile Population)")
 
-g20worst <- ggplot(data=mydata20, aes(x=mydata20$Zyklus, color=Kodierung))  + geom_line(aes(y=mydata20$worstBinaer1P, col="worstBinaer1P"))+ geom_line(aes(y=mydata20$worstBinaer2P, col="worstBinaer2P")) + geom_line(aes(y=mydata20$worstReal, col="worstReel")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
+g20worst <- ggplot(data=mydata20, aes(x=mydata20$Zyklus, color=Kodierung))  + geom_line(aes(y=mydata20$worstBinaer1P, col="worstBinaer1P"))+ geom_line(aes(y=mydata20$worstBinaer2P, col="worstBinaer2P")) + geom_line(aes(y=mydata20$worstReal, col="worstReal")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
 g20worst + theme() + theme(panel.grid.major = element_line(color = "black",
 linetype = "dotted"), legend.position = c(0.80,0.90)) + labs(x = "Zyklus",y =
  "Fitnesswert", title = "Mittlerer Fitnesswertverlauf mit 20 Genen (stabile Population)")
 
-g50worst <- ggplot(data=mydata50, aes(x=mydata50$Zyklus, color=Kodierung))  + geom_line(aes(y=mydata50$worstBinaer1P, col="worstBinaer1P"))+ geom_line(aes(y=mydata50$worstBinaer2P, col="worstBinaer2P")) + geom_line(aes(y=mydata50$worstReal, col="worstReel")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
+g50worst <- ggplot(data=mydata50, aes(x=mydata50$Zyklus, color=Kodierung))  + geom_line(aes(y=mydata50$worstBinaer1P, col="worstBinaer1P"))+ geom_line(aes(y=mydata50$worstBinaer2P, col="worstBinaer2P")) + geom_line(aes(y=mydata50$worstReal, col="worstReal")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
 g50worst + theme() + theme(panel.grid.major = element_line(color = "black",
 linetype = "dotted"), legend.position = c(0.80,0.90)) + labs(x = "Zyklus",y = "Fitnesswert", title = "Mittlerer Fitnesswertverlauf mit 50 Genen (stabile Population)")
 
@@ -56,7 +56,7 @@ g20 <- ggplot(data=mydata20, aes(x=mydata20$Zyklus, color=Kodierung)) + geom_lin
 g20 + theme() + theme(panel.grid.major = element_line(color = "black", linetype = "dotted"), legend.position=c(.80, .90)) + labs(x = "Zyklus",y = "Fitnesswert", title = "Fitnessverlauf mit 20 Genen (wachsende Population)")
 ggsave("../best-20-grow.jpeg")
 
-g50 <- ggplot(data=mydata50, aes(x=mydata50$Zyklus, color=Kodierung)) + geom_line(aes(y=mydata50$bester.Fitnesswert.Real, col="Reele Werte")) + geom_line(aes(y=mydata50$Binaer1P, col="Binär1P"))+ geom_line(aes(y=mydata50$Binaer2P, col = "Binär2P")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
+g50 <- ggplot(data=mydata50, aes(x=mydata50$Zyklus, color=Kodierung)) + geom_line(aes(y=mydata50$bester.Fitnesswert.Real, col="Reelle Werte")) + geom_line(aes(y=mydata50$Binaer1P, col="Binär1P"))+ geom_line(aes(y=mydata50$Binaer2P, col = "Binär2P")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
 g50 + theme() + theme(panel.grid.major = element_line(color = "black", linetype = "dotted"), legend.position=c(.80, .90)) + labs(x = "Zyklus",y = "Fitnesswert", title = "Fitnessverlauf mit 50 Genen (wachsende Population")
 
 g5worst <- ggplot(data=mydata5, aes(x=mydata5$Zyklus, color=Kodierung))  + geom_line(aes(y=mydata5$worstBinaer1P, col="worstBinaer1P"))+ geom_line(aes(y=mydata5$worstBinaer2P, col="worstBinaer2P")) + geom_line(aes(y=mydata5$worstReal, col="worstReal")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
@@ -65,7 +65,7 @@ g5worst + theme() + theme(panel.grid.major = element_line(color = "black", linet
 g20worst <- ggplot(data=mydata20, aes(x=mydata20$Zyklus, color=Kodierung))  + geom_line(aes(y=mydata20$worstBinaer1P, col="worstBinaer1P"))+ geom_line(aes(y=mydata20$worstBinaer2P, col="worstBinaer2P")) + geom_line(aes(y=mydata20$worstReal, col="worstReal")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
 g20worst + theme() + theme(panel.grid.major = element_line(color = "black", linetype = "dotted"), legend.position = c(0.80,0.90)) + labs(x = "Zyklus",y = "Fitnesswert", title = "Fitnessverlauf mit 20 Genen (wachsende Population")
 
-g50worst <- ggplot(data=mydata50, aes(x=mydata50$Zyklus, color=Codierung))  + geom_line(aes(y=mydata50$worstBinaer1P, col="worstBinaer1P"))+ geom_line(aes(y=mydata50$Binaer1P, col="bestBinaer1P")) + geom_line(aes(y=mydata50$worstReal, col="worstReel")) + geom_line(aes(y=mydata50$bester.Fitnesswert.Real, col="bestReel")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
+g50worst <- ggplot(data=mydata50, aes(x=mydata50$Zyklus, color=Codierung))  + geom_line(aes(y=mydata50$worstBinaer1P, col="worstBinaer1P"))+ geom_line(aes(y=mydata50$Binaer1P, col="bestBinaer1P")) + geom_line(aes(y=mydata50$worstReal, col="worstReal")) + geom_line(aes(y=mydata50$bester.Fitnesswert.Real, col="bestReal")) + scale_x_continuous(limits=limitsX, breaks=stepsX) + scale_y_continuous(limits=limitsY, breaks=stepsY)
 g50worst + theme() + theme(panel.grid.major = element_line(color = "black", linetype = "dotted"), legend.position = c(0.80,0.90)) + labs(x = "Zyklus",y = "Fitnesswert", title = "Bester und schlechtester Fitnessverlauf mit 50 Genen (wachsende Population)")
 
 mydata5 <- cbind (mydata5, "Range1P" = mydata5$worstBinaer1P - mydata5$Binaer1P)
